e12ea73bfd31bcf1f78b2055954b64e88b057d41
Michael Prather <mprather1@gmail.com>
2018-11-29 14:19:42 +0000

    removed files

--- state/reducers/index.js
+++ state/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import trees from './trees'
 import about from './about'
-import files from './files'
 
 export default combineReducers({
   trees,
-  about,
-  files
+  about
 })

ec4b8bf3c0c1dc8cbc0f8deeb5763c1429abfb59
Michael Prather <mprather1@gmail.com>
2018-12-06 17:24:37 +0000

    tweaked size of motion - spring

--- components/Clicker/index.js
+++ components/Clicker/index.js
@@ -1,7 +1,7 @@
+import {Motion, spring} from 'react-motion'
 import PropTypes from 'prop-types'
 import Wrapper from './wrapper'
 import theme from './theme'
-import {Motion, spring} from 'react-motion';
 
 const Clicker = ({ trees, toggle, increment = () => {}, decrement = () => {} }) => {
   let  value 
@@ -9,7 +9,7 @@ const Clicker = ({ trees, toggle, increment = () => {}, decrement = () => {} }) 
   if (trees.value === 0) { 
     value = 36
   } else {
-    value = (trees.value > 0) ? 72.5 : 0
+    value = (trees.value > 0) ? 76 : 0
   }
 
   return (
@@ -17,7 +17,7 @@ const Clicker = ({ trees, toggle, increment = () => {}, decrement = () => {} }) 
       <div className='counter'>Number &#10144; <span>{trees.value}</span></div>
       <button className='decrement' onClick={() => { decrement(1) }}>Decrement!</button>
       <button className='increment' onClick={() => { increment(1) }}>Increment!</button>
-      
+
       <Motion style={{x: spring(value)}}>
         {({x}) =>
           <div className="motion">
